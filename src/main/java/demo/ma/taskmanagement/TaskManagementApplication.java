896c8f6c6632707429b754fa89bea853cdaba0d2
Ihssane Aziz <ihssaaane.aziz@gmail.com>
2025-06-22 17:22:12 +0100

    adding docker files

--- src/main/java/demo/ma/taskmanagement/TaskManagementApplication.java
+++ src/main/java/demo/ma/taskmanagement/TaskManagementApplication.java
@@ -1,5 +1,6 @@
 package demo.ma.taskmanagement;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TaskManagementApplication {
 
 	public static void main(String[] args) {
+		// Load .env file
+		try {
+			Dotenv dotenv = Dotenv.configure().load();
+			dotenv.entries().forEach(entry ->
+					System.setProperty(entry.getKey(), entry.getValue())
+			);
+		} catch (Exception e) {
+			System.out.println("No .env file found, using system properties");
+		}
+
 		SpringApplication.run(TaskManagementApplication.class, args);
 	}
-
 }
